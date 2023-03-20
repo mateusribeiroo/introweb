2029fd91d4603aed95eef0ad020d3d580f657240
Mateus Ribeiro <ribeiromateus0042@gmail.com>
2023-03-20 09:46:26 -0300

    first commit

---
+++ src/components/cards/AnimeCard.vue
@@ -0,0 +1,28 @@
+<script>
+    import api from "../../services/api";
+    import { defineComponent, onMounted, ref } from "vue"
+
+    export default defineComponent({
+        name: "animeCard",
+        setup() {
+            const animes = ref([]);
+
+            const fetchAnimes = async () => api.get("/anime?limit=20").then((response) => {
+                console.log(response)
+            });
+
+            onMounted(fetchAnimes);
+        }
+    })
+
+</script>
+
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<style scoped>
+
+</style>

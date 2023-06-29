c6882df0da1304ff720cfddf79a69c3cd9c8be2a
Mateus Ribeiro <ribeiromateus0042@gmail.com>
2023-06-28 22:19:41 -0300

    almost there

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -15,10 +15,12 @@
 
 <template>   
 
+  <h2 class="title">Insira abaixo o nome do seu anime!</h2>
+
   <main>
     <form id="form" class="mb-3"  @submit.prevent="handleSearch">
     <label for="searchAnimeField" hidden class="form-label"></label>
-    <input placeholder="Pesquisar..." class="form-control" type="search" v-model="searchAnimeField" >
+    <input placeholder="Pesquisar..." class="form-control text-align-center" type="search" v-model="searchAnimeField" >
   </form>
 
   <div class="row row-cols-1 row-cols-md-3 g-4">
@@ -37,11 +39,20 @@
 <style>
 
   main{
-    
+    min-height: 100vh;
+  }
+
+  .title{
+    margin: 2% 0;
+    text-align: center;
   }
 
   #form{
     margin: auto 2em;
+  }
+
+  .text-align-center{
+    text-align: center;
   }
 
   .align-center{
--- src/views/AnimeView.vue
+++ src/views/AnimeView.vue
@@ -19,26 +19,28 @@ onMounted(fetchAnime)
 </script>
 
 <template>
+    <div class="main">
+        <div id="principal">
+            <img  alt="anime banner">
+            <h2>{{ anime.title }}</h2>
+        </div>
 
-    <div id="principal">
-        <img  alt="anime banner">
-        <h2>{{ anime.title }}</h2>
-    </div>
+        <div>
+            {{ anime.synopsis }}
+        </div>
 
-    <div>
-        {{ anime.synopsis }}
-    </div>
-
-    <div>
-        <h4>Eps: {{ anime.episodes }}</h4>
-        <h4>Nota: {{ anime.score }}</h4>
-    </div>
-   
+        <div>
+            <h4>Eps: {{ anime.episodes }}</h4>
+            <h4>Nota: {{ anime.score }}</h4>
+        </div>
+   </div>
 
 </template>
 
 
 
 <style>
-
+.main{
+    min-height: 100vh;
+}
 </style>

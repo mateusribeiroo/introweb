afacc4016010ccdd57736f1b438061295062e4cf
Mateus Ribeiro <ribeiromateus0042@gmail.com>
2023-07-26 11:21:44 -0300

    trying

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -1,31 +1,44 @@
 <template>
-    <nav class="align-center" aria-label="Page navigation example">
-        <ul class="pagination">
-          <li class="page-item">
-            <a class="page-link" href="#" aria-label="Previous">
-              <span aria-hidden="true">&laquo;</span>
-            </a>
-          </li>
-          <li class="page-item"><a class="page-link" href="#">1</a></li>
-          <li class="page-item"><a class="page-link" href="#">2</a></li>
-          <li class="page-item"><a class="page-link" href="#">3</a></li>
-          <li class="page-item">
-            <a class="page-link" href="#" aria-label="Next">
-              <span aria-hidden="true">&raquo;</span>
-            </a>
-          </li>
-        </ul>
-    </nav>
+  <nav class="align-center" aria-label="Page navigation example">
+      <ul class="pagination">
+        <li class="page-item">
+          <a class="page-link" href="#" aria-label="Previous">
+            <span aria-hidden="true">&laquo;</span>
+          </a>
+        </li>
+        
+        <li
+          v-for="i in qtde_pages" :key="i" 
+          class="page-item"
+          >
+          <a class="page-link" href="#">{{ i+1 }}</a>
+        </li>
+
+        <li class="page-item">
+          <a class="page-link" href="#" aria-label="Next">
+            <span aria-hidden="true">&raquo;</span>
+          </a>
+        </li>
+      </ul>
+  </nav>
 </template>
 
 <script setup>
+  import { computed } from 'vue';
 
+  let {total, per_page, page} = defineProps({
+    per_page: Number,
+    page: Number,
+    has_next_page: Boolean
+  });
+
+  let qtde_pages = computed(() => total/per_page)
 </script>
 
 <style>
   .align-center{
-      display: flex;
-      align-items: center;
-      justify-content: center;
+    display: flex;
+    align-items: center;
+    justify-content: center;
   }
 </style>
--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,21 +1,22 @@
 <script setup>
   import AnimeCard from './AnimeCard.vue';
-  import Pagination  from './Pagination.vue'
   import api from '../services/api';
   import { computed, onMounted, ref } from 'vue';
-  import { limitString } from '../util/strings/limitString';
-import PaginationVue from './Pagination.vue';
 
   let anime_list = ref([]); 
   let searchAnimeField = ref("");
-  let pagination = {};
-
-  const fetchAnimes = async () => api.get("/anime?limit=50").then((response) => {
-    anime_list.value = response.data.data;
-    console.log(anime_list.value);
-  });
-
-  onMounted(fetchAnimes);
+  let qtde_pages, has_next_page, page, per_page_var;
+  
+  async function fetchAnimes(page, per_page){ 
+    await api.get(`/anime?limit=${per_page}&order_by=rank&page=${page}`).then((response) => {
+      anime_list.value = response.data.data;
+      has_next_page = response.data.pagination;
+      qtde_pages = response.data.pagination.items.per_page/5;
+      per_page_var = response.data.pagination.items.per_page;
+    });
+  }
+  
+  onMounted(fetchAnimes(page | 1, per_page_var | 25));
   
   const filteredAnimes = computed(() => {
     if(anime_list.value && searchAnimeField.value){
@@ -24,9 +25,15 @@ import PaginationVue from './Pagination.vue';
     return anime_list.value
   })
 
+  function animeNextPage(page, per_page_var){
+    return fetchAnimes(page | 1, per_page_var | 25);
+  }
+
+  
 </script>
 
-<template>
+<template>   
+
   <div class="mb-3">
     <label for="searchAnimeField" hidden class="form-label"></label>
     <input placeholder="Pesquisar..." class="form-control" type="text" v-model="searchAnimeField">
@@ -41,5 +48,36 @@ import PaginationVue from './Pagination.vue';
     />
   </div>
 
-  <Pagination />
+
+  <nav class="align-center" aria-label="Page navigation example">
+      <ul class="pagination">
+        <li class="page-item">
+          <a class="page-link" href="#" aria-label="Previous">
+            <span aria-hidden="true">&laquo;</span>
+          </a>
+        </li>
+        
+        <li
+          v-for="i in qtde_pages" :key="i" 
+          class="page-item"
+          >
+          <a :on-click="animeNextPage(i, 25)" class="page-link" href="#">{{ i }}</a>
+        </li>
+
+        <li v-if="has_next_page" class="page-item">
+          <a class="page-link" href="#" aria-label="Next">
+            <span aria-hidden="true">&raquo;</span>
+          </a>
+        </li>
+      </ul>
+  </nav>
+
 </template>
+
+<style>
+  .align-center{
+      display: flex;
+      align-items: center;
+      justify-content: center;
+  }
+</style>
